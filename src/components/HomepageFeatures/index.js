0208166625513c42905fce1214c39a3453f5c13c
Evan Luo <luojiashao@gmail.com>
2023-03-09 16:21:57 -0500

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -9,8 +9,6 @@ const FeatureList = [
     description: (
       <>
         Redefine 主题非常简单易用。您可以使用它快速轻松地搭建自己的博客，配置毫不繁琐。
-        Hexo theme redefine is easy to use. You can use it to build your own blog quickly and easily.
-        Also, the configuration is simple and easy to understand.
       </>
     ),
   },
@@ -19,8 +17,7 @@ const FeatureList = [
     Svg: require('@site/static/img/moon.svg').default,
     description: (
       <>
-        Redefine 主题让您专注于内容，我们将为您完成其他琐碎的工作。快去享受您的写作吧。
-        Theme Redefine lets you focus on your content, and we&apos;ll do the chores. Go ahead and enjoy your writing.
+        Redefine 主题让您专注于内容，我们将为您完成其他琐碎的工作。
       </>
     ),
   },
@@ -30,7 +27,6 @@ const FeatureList = [
     description: (
       <>
         Redefine 主题经常更新。您可以及时获得最新的功能和修复。欢迎提交 PR 和 issue。
-        Theme Redefine updates frequently. You can get the latest features and bug fixes in time. Feel free to pull requests and send issues.
       </>
     ),
   },
